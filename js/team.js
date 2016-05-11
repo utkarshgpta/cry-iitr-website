dcdfa7a6953d7bb5810629db317e819939d9e72c
Utkarsh Gupta <utkarshg1408@gmail.com>
2016-05-11 17:16:01 +0530

    Refactored Team Section Code

---
+++ js/team.js
@@ -0,0 +1,121 @@
+arr = [
+{
+"name": "Vini Sharma",
+"url": "vini.jpg"
+},
+{
+"name": "Prateeksha Maurya",
+"url": "prateeksha.jpg"
+},
+{
+"name": "Sandeep Chaudhary",
+"url": "sandeep.jpg"
+},
+{
+"name": "Priyanka Bansal",
+"url": "priyanka.jpg"
+},
+{
+"name": "Nishtha Behal",
+"url": "nishtha.jpg"
+},
+{
+"name": "Diksha Nagpal",
+"url": "diksha.jpg"
+},
+{
+"name": "Pramod Agarwal",
+"url": "pramod.jpg"
+},
+{
+"name": "Ankur Chaurasia",
+"url": "ankur.jpg"
+},
+{
+"name": "Ankit Veer Singh",
+"url": "ankit.jpg"
+},
+{
+"name": "Prapti Rathod",
+"url": "prapti.jpg"
+},
+{
+"name": "Anita Kumari",
+"url": "anita.jpg"
+},
+{
+"name": "Ruchika Dandotia",
+"url": "ruchika.jpg"
+},
+{
+"name": "Ananya Singla",
+"url": "ananya.jpg"
+},
+{
+"name": "Sweta Panwar",
+"url": "sweta.jpg"
+},
+{
+"name": "Jatinder Singh",
+"url": "jatinder.jpg"
+},
+{
+"name": "Ajay Khatri",
+"url": "ajay.jpg"
+},
+{
+"name": "Akshay Sharma",
+"url": "akshay.jpg"
+},
+{
+"name": "Kanvi Goyal",
+"url": "kanvi.jpg"
+},
+{
+"name": "Kapil Mailk",
+"url": "kapil.jpg"
+},
+{
+"name": "Md Imran",
+"url": "imran.jpg"
+},
+{
+"name": "Utkarsh Gupta",
+"url": "Utkarsh.jpg"
+},
+{
+"name": "Sourav Tripathi",
+"url": "sourav.jpg"
+},
+{
+"name": "Prashant Ganiya",
+"url": "prashant.jpg"
+},
+{
+"name": "Benison Joseph",
+"url": "benison.jpg"
+},
+{
+"name": "Avinash Kumar",
+"url": "avinash.jpg"
+},
+{
+"name": "Gnanendar Reddy",
+"url": "gnanendar.jpg"
+},
+{
+"name": "Reshi Kiran",
+"url": "reshi.jpg"
+},
+{
+"name": "Priyatham",
+"url": "priyatham.jpg"
+},
+{
+"name": "Abhishek Kumar",
+"url": "abhishek-kumar.jpg"
+}
+];
+
+team_info(arr);
+modal_team_info(arr);
