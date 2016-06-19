c435501cb4411ae1546b0bb9b393f4c55bd48d6e
Utkarsh Gupta <utkarshg1408@gmail.com>
2016-06-19 14:45:09 +0530

    Refactored code for Facts Section

--- js/team.js
+++ js/team.js
@@ -140,6 +140,59 @@ recent_works = [
 }
 ];
 
+facts_display = [
+{"fact" : "Children constitute over one-third of India’s population of 1.21 billion people, which means India is home to 400 million children."
+},
+{"fact" : "Every sixth child in the world lives in India (Ministry of Statistics and Programme Implementation- MoSPI, 2012)"
+},
+{"fact" : "India has 10.12 million child labourers aged between 5 to 14 years (National Census 2011)"
+},
+{"fact" : "An estimated 11.6 lakh children die every year within one year of their birth due to lack of immunisation. (Ministry of Health and Family Welfare, Government of India)"
+},
+{"fact" : "Vaccine preventable diseases (VPDs) are still responsible for over 5 lakh deaths annually in India."
+},
+{"fact" : "As per Coverage Evaluation Survey (CES-2009), 61% of children aged 12-23 months in the country are Fully Immunized with all vaccines. Nearly 8% children in the same age group did not receive a single vaccine in 2009."
+},
+{"fact" : "A slight gender differential noted, with 62% of the male children having received full immunization, as against 60% of females (Ministry of Statistics and Programme Implementation (MOSPI), 2012"
+},
+{"fact" : "Roughly 50% of all working children are girls (data from government reports)"
+},
+{"fact" : "Out of the 400 million children in India, every second child is malnourished (National Family Health Survey III – NFHS, 2005-06)"
+},
+{"fact" : "In India the child sex ratio is at the lowest it has ever been with just 914 girls for every 1000 boys (Census, 2011)"
+},
+{"fact" : "Maternal Mortality Ratio (MMR) in India continues to be high at 212 per 100,000 live births (Sample Registration System - SRS, 2011)"
+},
+{"fact" : "Girls in India have 61% higher mortality than boys at age 1-4 years (National Family Health Survey III - NFHS)"
+},
+{"fact" : "In India 22% babies are born with low birth weight (National Family Health Survey III - NFHS)"
+},
+{"fact" : "In India about 55% of Scheduled Castes and Scheduled Tribes children under 3 years of age are underweight compared to about 37% of children from the general population of 400 million children (National Family Health Survey III - NFHS)"
+},
+{"fact" : "The Under-5 Mortality Rate (U5MR) in India is 88.1% for Schedule Caste and 95.7% for Schedule Tribe children, against the national average of 59.2% (National Family Health Survey III - NFHS)"
+},
+{"fact" : "In India 47 out of every 1000 live births do not complete their first year of life ((Sample Registration System - SRS, 2011)"
+},
+{"fact" : "79% children of the 400 million in India (6-35 months) are anaemic (National Family Health Survey III - NFHS)"
+},
+{"fact" : "56% adolescent girls (15-19 years) in India are anaemic, as against 30% adolescent boys (National Family Health Survey III - NFHS)"
+},
+{"fact" : "Only 54% children of the 400 million in India receive full immunization (District Level Household & Facility Survey III - DLHI, 2007-08)"
+},
+{"fact" : "Over 25% increase in child murders is noted since 2000 in India (National Crime Records Bureau - NCRB, 2011)"
+},
+{"fact" : "The Net Enrollment Ratio (NER) at the Upper Primary Elementary Level in government schools in India is only 58.3% (Ministry of Statistics and Programme Implementation - MoSPI, 2012)"
+},
+{"fact" : "Gross Enrollment Ratio (GER) at the Secondary Level in government schools in India is below 50% (District Information System for Education - DISE, 2011-12)"
+},
+{"fact" : "About 35% children in India with disabilities remain out of Elementary school (District Information System for Education - DISE, 2011-12)"
+},
+{"fact" : "School dropout rate amongst adolescent girls in India is as high as 63.5% (Ministry of Statistics and Programme Implementation - MoSPI, 2012)"
+},
+{"fact" : "Nearly 45% girls In India get married before the age of eighteen years (National Family Health Survey III - NFHS)"
+},
+];
+
 /* Team Section*/
 var width = screen.width;
 var columns = 6;
@@ -149,3 +202,6 @@ team_info(arr,Math.ceil(arr.length/columns),columns,"modal");
 
 /* Recent Works Section */
 recent(recent_works);
+
+/* Facts */
+facts(facts_display);
