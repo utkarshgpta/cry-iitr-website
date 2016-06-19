257e8313352fc56d03cb5fc0f3be3a1178fe93fd
Utkarsh Gupta <utkarshg1408@gmail.com>
2016-06-19 14:16:18 +0530

    Refactored Team Code and added Gluphocons

--- js/team.js
+++ js/team.js
@@ -124,36 +124,28 @@ arr = [
 recent_works = [
 {"news": "Fun Events organised for students who came to IIT Roorkee under the initiative of SIIRD IIT Roorkee",
 "url":"https://www.facebook.com/CRY.Roorkee/posts/1207664429260592",
-"date":""
+"date":"November 1,2015"
 },
-{"news":"Organisation of events like Sparsh and Pic-O-Mania in Thomso'15 to raise funds.",
+{"news":"Organisation of events like Sparsh and Pic-O-Mania in Thomso'15.",
 "url":"https://www.facebook.com/CRY.Roorkee/posts/1199998556693846",
-"date":""
-},
-{"news":"Survey in Khanjarpur",
-"url":"",
-"date":""
-},
-{"news":"Recruitment of new voluteers in the group",
-"url":"",
-"date":""
+"date":"October 16,2015"
 },
 {"news":"Donation of clothes collected from Sarojini and Kasturba Bhawan in Kasturba Gandhi Balika Vidhyala,Roorkee.",
 "url":"https://www.facebook.com/CRY.Roorkee/posts/1178184252208610",
-"date":""
+"date":"September 8, 2015"
 },
 {"news":"Drawing and Rakhi making competition held in KGBV on the occasion of Rakshabandhan",
-"url":"https://www.facebook.com/CRY.Roorkee/timeline/story?ut=43&wstart=0&wend=1448956799&hash=5034263663512705672&pagefilter=3",
-"date":""
-},
-{"news":"",
-"url":"",
-"date":""
-},
+"url":"https://www.facebook.com/CRY.Roorkee/posts/1176946315665737",
+"date":"September 6,2015"
+}
 ];
 
+/* Team Section*/
 var width = screen.width;
 var columns = 6;
 if (width<768)  columns = 4;
 team_info(arr,4,columns,"main");
 team_info(arr,Math.ceil(arr.length/columns),columns,"modal");
+
+/* Recent Works Section */
+recent(recent_works);
